f99c2bdcb53e7d2a8af9e21ecc7479a45579d652
Sourov Halder <sourov.eps@gmail.com>
2022-12-27 16:35:10 +0600

    Building processed configurations

--- Config-Builder/Config-Builder/Program.cs
+++ Config-Builder/Config-Builder/Program.cs
@@ -1,4 +1,8 @@
+using System.IO;
+using System.Text;
+using Config_Builder.Services.AppConfig;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Config_Builder
@@ -7,12 +11,26 @@ namespace Config_Builder
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilderWithCustomJson(args).Build().Run();
         }
 
+        // ReSharper disable once UnusedMember.Global
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+
+        // Build IHostBuilder with Custom Configuration
+        public static IHostBuilder CreateHostBuilderWithCustomJson(string[] args)
+        {
+            var appConfig = GetAppConfig.JsonMerger();
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, config) =>
+                {
+                    config.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(appConfig)));
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
         }
     }
